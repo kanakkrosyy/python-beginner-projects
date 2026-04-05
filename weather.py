dbf77778cef1751e99445d3344476042912b377d
kanak <kanakkrose@gmail.com>
2026-04-05 14:48:53 +0530

    Add files via upload

---
+++ weather.py
@@ -0,0 +1,12 @@
+#temperature 
+
+temperature = float(input("Enter the temperature : "))
+
+if temperature > 30 :
+    print ("it's a hot day")
+
+elif temperature < 10 :
+    print("its a cold day")
+
+else :
+    print("its neither hot or cold")
